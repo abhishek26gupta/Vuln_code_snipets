e99cd64e07adc1edfeab918fdce89a8348f7d953
Abhishek Gupta <105964982+abhishek26gupta@users.noreply.github.com>
2025-10-24 23:42:27 +0530

    Create JWT_Alg_none.py

---
+++ JWT_Alg_none.py
@@ -0,0 +1,28 @@
+import os
+import jwt
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+SECRET_KEY = os.getenv('JWT_AUTH_SECRET_KEY')
+
+@app.route('/admin', methods=['GET'])
+def admin_only():
+        token = request.headers.get('Authorization' , '') .replace('Bearer ', '')
+
+        try:
+                payload = jwt. decode(
+                        token,
+                        SECRET_KEY,
+                        options={"require": ["exp", "iat"], "verify_exp": True, "verify_signature": False}
+                )
+                if payload.get('admin'):
+                        return jsonify({"message": "Welcome to the admin panel", "data": " <!-- Sensitive data -- >"})
+
+                return jsonify({"error": "Unauthorized!"}), 403
+
+        except Exception as e:
+                return jsonify({"error": str(e)}), 401
+
+if __name__ == "__main__" :
+        app.run(host='0.0.0.0', port=5000, debug=True)
